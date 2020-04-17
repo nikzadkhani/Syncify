5c67efa44d644e0959fa40949a023a1ba55334ec
ParkerStone <parkerwstone@gmail.com>
2020-04-16 18:36:30 -0700

    added comments and documentation

--- spotify-prototype/api/DB.js
+++ spotify-prototype/api/DB.js
@@ -1,4 +1,6 @@
 module.exports = {
-    //DB: 'mongodb://localhost:27017/ng7crud'
+    // local DB url, need mongod running
+    //DB: 'mongodb://localhost:27017/syncify'
+    // mongo cloud DB url
     DB: 'mongodb+srv://syncify:cs411@cluster0-byvmy.azure.mongodb.net/syncify?retryWrites=true&w=majority'
  };
--- spotify-prototype/src/app/playlist.service.ts
+++ spotify-prototype/src/app/playlist.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+// This is the playlist service, call this for any backend needs.
+// See Playlist.ts and Song.ts for data definitions
 @Injectable({
   providedIn: 'root'
 })
@@ -10,6 +12,7 @@ export class PlaylistService {
 
   constructor(private http: HttpClient) { }
 
+  // This creates a new playlist with the given name
   addPlaylist(playlist_name) {
     const obj = {
       name: playlist_name,
@@ -19,18 +22,21 @@ export class PlaylistService {
         .subscribe(res => console.log('Done'));
   }
 
+  // This returns a list of all playlists
   getPlaylists() {
       return this
              .http
              .get(`${this.uri}`);
   }
 
+  // This returns the playlist object for the given playlist ID
   editPlaylists(id) {
       return this
               .http
               .get(`${this.uri}/edit/${id}`);
   }
 
+  // This updates the playlist for the given playlist ID
   updatePlaylist(id, playlist_name) {
 
       const obj = {
@@ -42,12 +48,14 @@ export class PlaylistService {
         .subscribe(res => console.log('Done'));
   }
 
+  // Deletes the playlist of the given playlist ID
   deletePlaylist(id) {
       return this
                 .http
                 .get(`${this.uri}/delete/${id}`);
   }
 
+  // This adds a song to the playlist of 'id', must provide song name, artist, and album
   addSongToPlaylist(id, song_name, song_artist, song_album) {
     const obj = {
       song_name: song_name,
@@ -60,6 +68,7 @@ export class PlaylistService {
       .subscribe(res => console.log('Done'));
   }
 
+  // This removes a song from the playlist 'id' and song id
   removeSongFromPlaylist(id, song_id) {
     this
       .http
