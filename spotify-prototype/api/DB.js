611581c018bef52ca3778883e7ded5d8d7a92130
ParkerStone <parkerwstone@gmail.com>
2020-04-16 18:21:40 -0700

    changed database user to a syncify database user

--- spotify-prototype/api/DB.js
+++ spotify-prototype/api/DB.js
@@ -1,4 +1,4 @@
 module.exports = {
     //DB: 'mongodb://localhost:27017/ng7crud'
-    DB: 'mongodb+srv://pstone:syncify@cluster0-byvmy.azure.mongodb.net/syncify?retryWrites=true&w=majority'
+    DB: 'mongodb+srv://syncify:cs411@cluster0-byvmy.azure.mongodb.net/syncify?retryWrites=true&w=majority'
  };
