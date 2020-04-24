b089ceef282897fb557924efa73ecaf123da2390
Mark White <44020053+mwhite317@users.noreply.github.com>
2020-04-24 10:23:57 +0200

    swtich to constructor binding

--- src/main/java/com/syncify/app/service/AppleMusic.java
+++ src/main/java/com/syncify/app/service/AppleMusic.java
@@ -21,14 +21,16 @@ public class AppleMusic {
 
     private final Logger log = LoggerFactory.getLogger(AppleMusic.class);
 
-    @Value("${secret.key-id}")
     private String keyId;
-
-    @Value("${secret.team-id}")
     private String teamId;
-
-    @Value("${secret.private-key}")
     private String privateKey;
+
+    public AppleMusic(@Value("${secret.key-id}") String keyId, @Value("${secret.team-id}") String teamId, @Value("${secret.private-key}") String privateKey) {
+        this.keyId = keyId;
+        this.teamId = teamId;
+        this.privateKey = privateKey;
+    }
+
 
     private String appleApiUrl = "https://api.music.apple.com/v1/catalog/us/search?term=";
 
