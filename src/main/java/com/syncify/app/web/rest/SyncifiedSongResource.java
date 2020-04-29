4c6ece5bfbc569266a33223db9c1f35c3f36dde9
Mark White <44020053+mwhite317@users.noreply.github.com>
2020-04-29 18:10:13 +0200

    create simple cache for Apple Music and wire it into Rest resource

--- src/main/java/com/syncify/app/web/rest/SyncifiedSongResource.java
+++ src/main/java/com/syncify/app/web/rest/SyncifiedSongResource.java
@@ -4,6 +4,7 @@ import com.syncify.app.domain.Song;
 import com.syncify.app.domain.SongRequest;
 import com.syncify.app.repository.SongRepository;
 import com.syncify.app.service.AppleMusicWeb;
+import com.syncify.app.service.CachedAppleMusic;
 import com.syncify.app.service.SpotifyMusic;
 
 import io.github.jhipster.web.util.HeaderUtil;
@@ -39,10 +40,13 @@ public class SyncifiedSongResource {
 
     private SpotifyMusic spotifyMusic;
 
+    private CachedAppleMusic cachedAppleMusic;
+
     public SyncifiedSongResource(SongRepository songRepository, AppleMusicWeb appleMusic, SpotifyMusic spotifyMusic) {
         this.songRepository = songRepository;
         this.appleMusic = appleMusic;
         this.spotifyMusic = spotifyMusic;
+        this.cachedAppleMusic = new CachedAppleMusic(appleMusic, songRepository);
     }
 
     /**
@@ -55,7 +59,7 @@ public class SyncifiedSongResource {
     @PostMapping("/syncifiedSongs")
     public ResponseEntity<Song> createSong(@RequestBody SongRequest songRequest) throws URISyntaxException {
         log.debug("REST request to save Song : {}", songRequest);
-        Song song = appleMusic.getSongFromSearchTerm(songRequest.getName());
+        Song song = cachedAppleMusic.getSongFromSearchTerm(songRequest.getName());
         spotifyMusic.updateSongWithSpotifyURL(song);
         song.setSyncifyId(songRequest.getSyncifyId());
 
